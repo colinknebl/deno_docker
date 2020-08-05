6119dcf21befee46614379d4ec61cdc68b30a625
Colin Knebl <colinknebl@outlook.com>
2020-08-04 22:32:35 -0400

    updated message

--- src/Router.ts
+++ src/Router.ts
@@ -10,7 +10,7 @@ books.set("1", {
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Hello world, from the api!";
+    context.response.body = "Hello world, from the api! BooYah!";
   })
   .get("/books", (context) => {
     context.response.body = Array.from(books.values());
