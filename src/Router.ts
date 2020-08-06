08e10d905b97c780c70692522be5d98d9240ceec
Colin Knebl <colinknebl@outlook.com>
2020-08-05 20:37:15 -0400

    update message again

--- src/Router.ts
+++ src/Router.ts
@@ -13,7 +13,7 @@ router
     context.response.body = "Welcome, this is the API skeleton.";
   })
   .get("/pipeline-test-message", (context) => {
-    context.response.body = "Pipeline test message! WooHoo!";
+    context.response.body = "Pipeline test message! Boom, baby!";
   })
   .get("/books", (context) => {
     context.response.body = Array.from(books.values());
